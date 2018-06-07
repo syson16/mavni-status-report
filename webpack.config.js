228a91ebda69cd2f6c196e1735f98330e36dd31e
Sangyeon Son <sangyeonson@Sangyeons-iMac.home>
2018-06-06 23:44:56 -0400

    add june table

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,12 @@ const path = require('path');
 
 module.exports = {
 	mode: 'production',
-  entry: './src/may.js',
-  output: {
-    filename: 'may.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+  	entry: {
+  		may: './src/may.js',
+  		june: './src/june.js'
+  	},
+  	output: {
+		filename: '[name].js',
+		path: path.resolve(__dirname, 'dist')
+	}
 };

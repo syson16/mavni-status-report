da3f37b9c434b1a6325f9edd0eee80311e706e6e
Sangyeon Son <sangyeonson@Sangyeons-iMac.home>
2018-06-08 11:41:18 -0400

    add the horizontal bar

--- src/june.js
+++ src/june.js
@@ -5,14 +5,19 @@ import dt from 'datatables';
 
 import 'datatables.net-plugins/sorting/datetime-moment.js';
 
+import Chart from 'chart.js';
+import _ from 'lodash';
 
 const data = require('./json/june.json');
 
 $(document).ready(function() {
 
-	$.fn.dataTable.moment('M/D/YY');
 
-	// Initiate the datatable    
+    // DATATABLES
+
+	$.fn.dataTable.moment('M/D/YY');
+    
+    // Initiate the datatable    
     $('#entire-table').DataTable( {
         data: data,
         "columns": [
@@ -30,5 +35,79 @@ $(document).ready(function() {
             { "data": "USCISnotifiedDate" }
         ]
     } );
+
+
+    // CHART.JS
+
+    var color = Chart.helpers.color;
+
+    // Horizontal Bar
+
+    var jan = _.filter(data, function(obj){
+        return moment(obj.mssd, 'M/D/YY').month() === 0;
+    });
+    var feb = _.filter(data, function(obj){
+        return moment(obj.mssd, 'M/D/YY').month() === 1;
+    });
+    var mar = _.filter(data, function(obj){
+        return moment(obj.mssd, 'M/D/YY').month() === 2;
+    });
+    var apr = _.filter(data, function(obj){
+        return moment(obj.mssd, 'M/D/YY').month() === 3;
+    });
+    var may = _.filter(data, function(obj){
+        return moment(obj.mssd, 'M/D/YY').month() === 4;
+    });
+    
+    var mssdWaiting = _.filter(data, function(obj){
+        return obj.mssd === '';
+    });
+    var mssdPending = _.filter(data, function(obj){
+        return obj.mssd === 'Pend ng';
+    });
+
+    $('#mssd-waiting').html(mssdWaiting.length);
+    $('#mssd-pending').html(mssdPending.length);
+
+    var horizontalBarChartData = {
+        labels: ['January', 'February', 'March', 'April', 'May'],
+        datasets: [{
+            label: 'Dataset',
+            backgroundColor: color(window.chartColors.red).alpha(0.5).rgbString(),
+            borderColor: window.chartColors.red,
+            borderWidth: 1,
+            data: [
+                jan.length,
+                feb.length,
+                mar.length,
+                apr.length,
+                may.length,
+            ]
+        }]
+
+    };
+
+    var ctx = document.getElementById('canvas').getContext('2d');
+    var myHorizontalBar = new Chart(ctx, {
+        type: 'horizontalBar',
+        data: horizontalBarChartData,
+        options: {
+            // Elements options apply to all of the options unless overridden in a dataset
+            // In this case, we are setting the border of each horizontal bar to be 2px wide
+            elements: {
+                rectangle: {
+                    borderWidth: 2,
+                }
+            },
+            responsive: true,
+            legend: {
+                position: 'right',
+            },
+            title: {
+                display: true,
+                text: '# of MSSD Completed by Month'
+            }
+        }
+    });
 
 });
